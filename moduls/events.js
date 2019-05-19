33c2993d26112c483bc1c5e16d717231378f13b6
Ilya S <frilans.er@mail.ru>
2019-05-19 19:44:36 +1100

    Создал событие, которое доступно только для объектов, созданных с помощью конструктора объектов Cars

--- moduls/events.js
+++ moduls/events.js
@@ -9,3 +9,29 @@ myEmit.on("myEvent", (text) => {
 
 // Принудительно вызвать действие
 myEmit.emit("myEvent", "Мое событие сработало");
+
+// Разбираю модуль унаследования события для всех объектов
+let util = require("util");
+
+let Cars = function(model) {
+    this.model = model;
+}
+
+// Для всех объектов созданных через "Cars" будет использоваться данное событие
+util.inherits(Cars, events.EventEmitter);
+
+let bmw = new Cars("BMW"),
+    audi = new Cars("Audi"),
+    volvo = new Cars("Volvo");
+
+let cars = [bmw, audi, volvo];
+cars.forEach((car) => {
+    car.on('speed', (text) => {
+        console.log(`${car.model} speed is - ${text}`);
+    });
+});
+
+// Событие доступно для всех последующих обьектов конструктора Cars
+bmw.emit("speed", "256.2 km");
+audi.emit("speed", "230.2 km");
+volvo.emit("speed", "180.2 km");
