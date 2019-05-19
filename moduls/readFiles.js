12d1c5a02fec307d3fd1ca01b11319b0d2579aac
Ilya S <frilans.er@mail.ru>
2019-05-19 22:51:02 +1100

    Заменил синхронное чтение/создание файлов, на асинхронное

--- moduls/readFiles.js
+++ moduls/readFiles.js
@@ -2,9 +2,17 @@ let fs = require("fs");
 
 // Синхронное чтение файла (подобный путь, так как файл берется относит. index)
 let way = "./moduls/source/";
-let fileReaded = fs.readFileSync(`${way}testInfo.txt`, 'utf8');
+// let fileReaded = fs.readFileSync(`${way}testInfo.txt`, 'utf8');
 // console.log(`Проверить файл: ${fileReaded}`);
 
 // Синхронное создание файла (так же возможность записи в файл)
-let infoForNewFile = `${fileReaded}\n Да, здравствует новый файл`;
-fs.writeFileSync(`${way}testInfoNew.txt`, infoForNewFile);
+// let infoForNewFile = `${fileReaded}\n Да, здравствует новый файл`;
+// fs.writeFileSync(`${way}testInfoNew.txt`, infoForNewFile);
+
+// Асинхронная чтение/запись файла с каллбэком
+fs.readFile(`${way}testInfo.txt`, "utf8", (err, data) => {
+    console.log(`Файл был прочитан, результат: ${data}`);
+});
+fs.writeFile(`${way}testInfoNew.txt`, "Hi asych write", (err, data) => {
+    console.log("Файл был создан/записан");
+});
